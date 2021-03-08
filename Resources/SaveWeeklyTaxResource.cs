384f3f26aafcde4aa962ff894b5413f3c3038d06
Vaidotas Cibulskas <v.cibulskas@gmail.com>
2021-03-08 23:43:48 +0200

    Fix week number validation

--- Resources/SaveWeeklyTaxResource.cs
+++ Resources/SaveWeeklyTaxResource.cs
@@ -6,7 +6,7 @@ namespace Nordic.Taxes.Resources
 	public class SaveWeeklyTaxResource : BaseSaveTaxResource
 	{
 		[Required]
-		[Range(1, 12,
+		[Range(1, 53,
 			ErrorMessage = "Value for {0} must be between {1} and {2}.")]
 		public short WeekNumber { get; set; }
 
